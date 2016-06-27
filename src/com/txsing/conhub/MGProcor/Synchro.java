1a88c848ffe4c30c31002c80ef86032c26c7bf41
txsing <txsing@gmail.com>
2016-06-27 13:22:09 +0800

    -m repo sync v0.3

--- src/com/txsing/conhub/MGProcor/Synchro.java
+++ src/com/txsing/conhub/MGProcor/Synchro.java
@@ -9,6 +9,10 @@ import com.txsing.conhub.ult.*;
 import com.txsing.conhub.object.*;
 import java.sql.*;
 import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -65,66 +69,92 @@ public class Synchro {
 
             JSONObject repoJSONObject = getRepoJSONInfo();
 
-            List<String> repoLst = getRepoListFromDB(regName, conn);
+            List<List<String>> repoDBLst = getRepoListFromDB(regName, conn);
 
             for (Object repokey : repoJSONObject.keySet()) {
-                String repoID = (String) repokey;
-
+                String repoName = (String) repokey;
+                System.err.println(regName + ":" + repoName);
                 //e,g,. Ubuntu:14.03, here Ubuntu is the repo name while 14.03 is tag.
                 //First check whether Ubuntu Repo is already stored in DB or not,
                 //if ture, then check whether tag-"14.03" exists or not. 
-                if (repoLst.contains(repoID)) {
+                int index = repoDBLst.get(0).indexOf(regName + ":" + repoName);
+                if (index != -1) {
+                    String repoID = repoDBLst.get(1).get(index);
                     List<String> tagDBLst = getImageTagListFromDB(repoID, conn);
 
                     JSONObject tagJSONObject = (JSONObject) repoJSONObject
-                            .get(repoID);
+                            .get(repoName);
 
                     for (Object tagkey : tagJSONObject.keySet()) {
                         String tag = (String) tagkey;
-                        if (!tagDBLst.contains(tag)) { //14.03 not existed
-                            insertNewTagIntoDB(conn, tag,
-                                    tagJSONObject.get(tag).toString(), repoID);
+                        String tagAfterProcess
+                                = tag.substring(tag.indexOf(":") + 1);
+
+                        if (!tagDBLst.contains(tagAfterProcess)) { //14.03 not existed
+                            System.err.println("Insert Tag: " + tagAfterProcess);
+
+                            String imageID = tagJSONObject.get(tag).toString();
+                            imageID = imageID.substring(imageID.indexOf("sha") + 7);
+
+                            insertNewTagIntoDB(conn, tagAfterProcess,
+                                    imageID, repoName);
                         }
                     }
                 } else {
-                    insertNewRepoIntoDB(conn, repoID, regName);
+                    insertNewRepoIntoDB(conn, repoName, regName);
+                    System.err.println("Insert Repo: " + regName + ":" + repoName);
                     JSONObject tagJSONObject = (JSONObject) repoJSONObject
-                            .get(repoID);
+                            .get(repoName);
                     for (Object tagkey : tagJSONObject.keySet()) {
                         String tag = (String) tagkey;
-                        insertNewTagIntoDB(conn, tag,
-                                tagJSONObject.get(tag).toString(), repoID);
+                        String tagAfterProcess
+                                = tag.substring(tag.indexOf(":") + 1);
+
+                        System.err.println("Insert Tag: " + tagAfterProcess);
+
+                        String imageID = tagJSONObject.get(tag).toString();
+                        imageID = imageID.substring(imageID.indexOf("sha") + 7);
+
+                        insertNewTagIntoDB(conn, tagAfterProcess,
+                                imageID, repoName);
 
                     }
                 }
             }
 
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
-    
 //####################################REPO DB DAO#############################\\
-    private static List<String> getRepoListFromDB(String registryName, 
+    private static List<List<String>> getRepoListFromDB(String registryName,
             Connection conn) {
+        String getRepoSQL = getRepoSQL = "SELECT * FROM repositories WHERE"
+                + " regname = '" + registryName + "'";
         try {
-            String getRepoSQL = "SELECT repoid FROM repositories WHERE "
-                    + "regname = '" + registryName + "'";            
-            System.err.println("Get Repo SQL: "+getRepoSQL);
-            
             Statement stmt = conn.createStatement();
             ResultSet repoRs = stmt.executeQuery(getRepoSQL);
 
             List<String> repoLst = new ArrayList<>();
+            List<String> repoIDLst = new ArrayList<>();
+
             while (repoRs.next()) {
-                repoLst.add(repoRs.getString("1"));
+                repoLst.add(repoRs.getString(3) + ":" + repoRs.getString(2));
+                repoIDLst.add(repoRs.getString(1));
             }
+
+            List<List<String>> result = new ArrayList<>();
+            result.add(repoLst);
+            result.add(repoIDLst);
+
             stmt.close();
             repoRs.close();
 
-            return repoLst;
+            return result;
         } catch (Exception e) {
+            System.err.println("Get Repo SQL: " + getRepoSQL);
             e.printStackTrace();
             return null;
         }
@@ -132,34 +162,34 @@ public class Synchro {
 
     private static boolean insertNewRepoIntoDB(Connection conn,
             String repoName, String regName) {
+        String sql = null;
         try {
-            SimpleDateFormat formater = new SimpleDateFormat("YYMMddHHmmss");
+            SimpleDateFormat formater = new SimpleDateFormat("YYMMddHHmmssS");
             String repoID = formater.format(Calendar.getInstance().getTime());
             repoID = "RP" + repoID;
 
-            String sql = "INSERT INTO repositories VALUES("
-                    + repoID + "', '" + repoName + "', '" + regName + "'";
+            sql = "INSERT INTO repositories VALUES('"
+                    + repoID + "', '" + repoName + "', '" + regName + "')";
 
             Statement stmt = conn.createStatement();
-            stmt.executeQuery(sql);
+            stmt.executeUpdate(sql);
             stmt.close();
             return true;
         } catch (Exception e) {
+            System.err.println("INSERT REPO: " + sql);
             e.printStackTrace();
         }
         return false;
     }
 
-    
-//#############################TAG DB DAO#####################################\\
+//#############################Tag DB DAO#####################################\\
     private static List<String> getImageTagListFromDB(String repoid,
             Connection conn) {
+        String sql = "SELECT tag FROM tags WHERE repoid = '"
+                + repoid + "'";
         try {
             List<String> resultLst = new ArrayList<>();
-            String sql = "SELECT tag FROM tags WHERE repoid = '"
-                    + repoid;
-            System.err.println("Get Tag SQL: "+sql);
-            
+
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
 
@@ -171,6 +201,7 @@ public class Synchro {
 
             return resultLst;
         } catch (Exception e) {
+            System.err.println("Get Tag SQL: " + sql);
             e.printStackTrace();
             return null;
         }
@@ -178,22 +209,21 @@ public class Synchro {
 
     private static boolean insertNewTagIntoDB(Connection conn, String tag,
             String imageID, String repoID) {
-        String sql = "INSERT INTO tags VALUES("
-                + tag + "', '" + imageID + "', '" + repoID + "'";
-
+        String sql = "INSERT INTO tags VALUES('"
+                + tag + "', '" + imageID + "', '" + repoID + "')";
         try {
             Statement stmt = conn.createStatement();
-            stmt.executeQuery(sql);
+            stmt.executeUpdate(sql);
             stmt.close();
             return true;
         } catch (Exception e) {
+            System.err.println("INSERT TAG: " + sql);
             e.printStackTrace();
         }
         return false;
     }
 
-    
-//############################IMAGE DB DAO####################################\\
+//################################Image DB DAO################################\\
     private static boolean insertNewImageIntoDB(String imageID,
             Connection conn) {
         try {
@@ -214,6 +244,7 @@ public class Synchro {
 
             return true;
         } catch (Exception e) {
+
             e.printStackTrace();
             return false;
         }
@@ -316,8 +347,10 @@ public class Synchro {
 
     private static JSONObject getRepoJSONInfo() {
         JSONObject jsonObject;
+
         String[] cmdParaArray
-                = {"cat", "/var/lib/docker/image/aufs/repositories.json"};
+                = {"/bin/bash", "-c", "echo scse | sudo -S "
+                    + "cat /var/lib/docker/image/aufs/repositories.json"};
         try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             CmdExecutor.executeNonInteractiveDockerCMD(cmdParaArray, baos);
@@ -325,12 +358,13 @@ public class Synchro {
 
             //remove the firstCotd pair of bracket, [ {xxxxx} ]
             imageInfoJsonString = imageInfoJsonString.
-                    substring(1, imageInfoJsonString.lastIndexOf(']'));
-            //System.err.println(imageInfoJsonString);
+                    substring(imageInfoJsonString.indexOf("{"));
 
+            //System.err.println(imageInfoJsonString);
             baos.close();
             jsonObject = (JSONObject) new JSONParser()
                     .parse(imageInfoJsonString);
+            jsonObject = (JSONObject) jsonObject.get("Repositories");
             return jsonObject;
         } catch (Exception e) {
             e.printStackTrace();
--- src/com/txsing/conhub/MGProcor/main.java
+++ src/com/txsing/conhub/MGProcor/main.java
@@ -7,6 +7,9 @@ package com.txsing.conhub.MGProcor;
 
 import java.util.*;
 import com.txsing.conhub.ult.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,37 +18,48 @@ import com.txsing.conhub.ult.*;
 public class main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        while (scanner.hasNext()) {
-            String cmd = scanner.nextLine();
-            List<String> output = executeCMD(cmd);
-            for (String str : output) {
-                System.out.println(str);
-            }
-        }
-        scanner.close();
+//        Scanner scanner = new Scanner(System.in);
+//        while (scanner.hasNext()) {
+//            String cmd = scanner.nextLine();
+//            List<String> output = executeCMD(cmd);
+//            for (String str : output) {
+//                System.out.println(str);
+//            }
+//        }
+//        scanner.close();
+        Synchro.syncRepo();
     }
-
+    
     public static List<String> executeCMD(String cmd) {
         return CmdExecutor.execute(cmd);
+    }
+    
+    public void setLogger() {
+        try {
+            Logger logger = Logger.getLogger("com.txsing.conhub.mgprocor");
+            FileHandler fileHandler = new FileHandler("mgprocor.log");
+            fileHandler.setLevel(Level.FINER);
+
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+
+        }
     }
 
     public static void startFileWatchService() {
         try {
-//            DockerFileWatcher imageWatcher
-//                    = new DockerFileWatcher(Constants.DOCKER_PATH_IMAGE,
-//                            "image", false);
-//            imageWatcher.start();
-//
-//            DockerFileWatcher containerWatcher
-//                    = new DockerFileWatcher(Constants.DOCKER_PATH_CONTAINER,
-//                            "container", false);
-//            containerWatcher.start();
-            
+            DockerFileWatcher imageWatcher
+                    = new DockerFileWatcher(Constants.DOCKER_PATH_IMAGE,
+                            "image", false);
+            imageWatcher.start();
+
+            DockerFileWatcher containerWatcher
+                    = new DockerFileWatcher(Constants.DOCKER_PATH_CONTAINER,
+                            "container", false);
+            containerWatcher.start();
+
             DockerFileWatcher repoWatcher
-                    = new DockerFileWatcher(Constants.DOCKER_PATH_REPOSITORY
-                            , "repo", false);
+                    = new DockerFileWatcher(Constants.DOCKER_PATH_REPOSITORY, "repo", false);
             repoWatcher.start();
 
         } catch (Exception e) {
